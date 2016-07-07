1c75521045422e95ca45048cf397ac2d087b31be
Calvin Ku <calvin.j.ku@googlemail.com>
2016-07-08 00:42:04 +0800

    changed test_forecaster -> test_data_etl.py

--- forecaster.py
+++ forecaster.py
@@ -30,11 +30,30 @@ pd.set_option('display.max_columns', 200)
 
 class Forecaster:
 	def __init__(self, symbol, dataloader):
+		self.label_ready = False
+		self.label_name = None
+
+		self.symbol = symbol
 		self.e = ETL(symbol, dataloader)
 		self.x_days = None
+		self.df_main = e.df_temp
+
 
 	def set_x_days(self, x):
 		self.x_days = x
+
+	def make_label(self):
+		if self.x_days is None or type(self.x_days) is not type(1):
+			print("Please set how far in the future would you like to forecast.")
+
+		else if self.label_ready:
+			self.df_main.drop([self.label_name], axis=1, inplace=True)
+
+		else:
+			self.label_ready = True
+			self.label_name = self.symbol + str(self.x_days) + 'd'
+			self.df_main[self.label_name] = self.df_main.shift(-5)[self.symbol]
+
 
 
 
